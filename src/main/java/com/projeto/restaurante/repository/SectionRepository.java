d2da43e02be6b21754f61e1cc166b6450fe6aeb0
Paulo Manzano <93448139+manzano-pje@users.noreply.github.com>
2024-11-23 14:32:21 -0300

    feature/TASK-019: Criação update section

--- src/main/java/com/projeto/restaurante/repository/SectionRepository.java
+++ src/main/java/com/projeto/restaurante/repository/SectionRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface SectionRepository extends JpaRepository<Section, Integer> {
     Optional<Section> findByName(String name);
+    Section findSectionByName(String name);
 }
